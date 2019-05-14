4a38136fced6c0c32d7e8493d1d8b7a28b1461e1
rango.lee@derivco.com.hk <rango.lee@derivco.com.hk>
2019-05-14 17:45:11 +0800

    more img

--- javascript/austin/main.js
+++ javascript/austin/main.js
@@ -1,17 +1,19 @@
 
 import {
-    getHomePageImages
+    getHomePageImages, getImages, imageBase
 } from "./getImage.js";
 import { draw } from "./draw.js";
 
 export const canvas = document.querySelector("canvas");
 export const content = canvas.getContext("2d");
+export const image = getImages(imageBase.avenger);
 
-runAnimation();
-export const homeImages = getHomePageImages();
+console.log(image);
 
 function runAnimation() {
     console.log("hello world");
     requestAnimationFrame(draw);
     requestAnimationFrame(runAnimation);
 }
+
+runAnimation();
