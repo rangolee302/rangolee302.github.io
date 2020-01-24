b6749e5ccfaf1da75609be903d6b50263a9a50c3
Lee <rangol@derivco.com.hk>
2020-01-24 10:35:38 +0800

    remove three js files

--- src/control/index.js
+++
@@ -1,5 +0,0 @@
-import * as THREE from "three";
-import ThreeOrbitControls from "three-orbit-controls";
-// import * as ThreeOrbitControls from "three-orbit-controls";
-export const OrbitControls = ThreeOrbitControls(THREE);
-// console.info({ THREE, ThreeOrbitControls, OrbitControls, });
